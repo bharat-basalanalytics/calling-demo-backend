4f543282e9502fe0bf007b1546de33664e9e8e9f
rozodkarbharat <rozodkarbharat@gmail.com>
2026-05-21 22:27:52 +0530

    wip

--- services/bullboard/index.js
+++ services/bullboard/index.js
@@ -1,5 +1,5 @@
 const { createBullBoard } = require('@bull-board/api')
-const { BullMQAdapter } = require('@bull-board/api/bullMQAdapter')
+// const { BullMQAdapter } = require('@bull-board/api/bullMQAdapter')
 const { ExpressAdapter } = require('@bull-board/express')
 
 const serverAdapter = new ExpressAdapter()
